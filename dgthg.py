88078df6c2906bc097053bb0772bdf4f86170702
kafankhauser <95697354+kafankhauser@users.noreply.github.com>
2022-01-19 20:55:11 +0100

    Update dgthg.py

--- dgthg.py
+++ dgthg.py
@@ -1,16 +1,51 @@
-Python 3.9.6 (v3.9.6:db3ff76da1, Jun 28 2021, 11:49:53) 
-[Clang 6.0 (clang-600.0.57)] on darwin
-Type "help", "copyright", "credits" or "license()" for more information.
-
-#change working directory
-import os
-cwd = os.getcwd()
-print("Current working directory: {0}".format(cwd))
-os.chdir('Users/kathi/Desktop/dgthg')
-cwd = os.getcwd()
-print("Current working directory: {0}".format(cwd))
-
 #import data set
 import pandas as pd
+df = pd.read_excel ("/Users/kathi/Desktop/dgthg/TAA_prox.xlsx")
+df
 
-df = pd.read_excel ('/Users/kathi/Desktop/dgthg/TAA_Kathi_DGTHG_anonymisiert.xlsx')
+#proximale landing zone CAVE LZ 1 sind eig LZ 1 und 0
+df["prox_landing_zone"].value_counts()
+
+# descriptive statistics - proximale landing zone
+df[["Aneur_diam", "Aneur_volume", "PDLZ_length", "OVP_diam"]].describe()
+
+# survival analysis
+from lifelines import KaplanMeierFitter
+kmf_survival = KaplanMeierFitter()
+kmf_survival.fit(df["Months_Surv_FU"], df["Survival"], label='Survival Kaplan Meier Estimate')
+kmf_survival.plot()
+
+#reintervention analysis
+# the data on reintervention includes NaNs in one row, index = 75
+df_reintervention = df
+df_reintervention = df_reintervention.drop(labels=75, axis = 0)
+
+
+kmf_reintervention = KaplanMeierFitter()
+kmf_reintervention.fit(df_reintervention["Months_Reinterv_FU"], df_reintervention["Reinterv"], label='Reintervention Kaplan Meier Estimate')
+kmf_reintervention.plot()
+
+
+
+# univariable analysis - cox regression Age, operative urgency, duration, Euroscore 2, post OP Neuro event
+from lifelines import CoxPHFitter
+cph_age = CoxPHFitter()
+cph_age.fit(df[["Age", "Months_Surv_FU", "Survival"]], duration_col="Months_Surv_FU", event_col= "Survival")
+cph_age.print_summary()  
+
+cph_duration = CoxPHFitter()
+cph_duration.fit(df[["Duration", "Months_Surv_FU", "Survival"]], duration_col="Months_Surv_FU", event_col= "Survival")
+cph_duration.print_summary() 
+
+cph_urgency = CoxPHFitter()
+cph_urgency.fit(df[["operative_urgency", "Months_Surv_FU", "Survival"]], duration_col="Months_Surv_FU", event_col= "Survival")
+cph_urgency.print_summary() 
+
+cph_neuro = CoxPHFitter()
+cph_neuro.fit(df[["post_OP_neuro_event", "Months_Surv_FU", "Survival"]], duration_col="Months_Surv_FU", event_col= "Survival")
+cph_neuro.print_summary() 
+
+cph_euroscore2 = CoxPHFitter()
+df_neuro = df[df['Euroscore2'].notna()]
+cph_euroscore2.fit(df_neuro[["Euroscore2", "Months_Surv_FU", "Survival"]], duration_col="Months_Surv_FU", event_col= "Survival")
+cph_euroscore2.print_summary() 
