b61b7cdb41ffcddb589e3ed7af5ea48ccaa809d7
kafankhauser <95697354+kafankhauser@users.noreply.github.com>
2022-02-02 12:26:17 +0100

    Update house_prizing.py

--- house_prizing.py
+++ house_prizing.py
@@ -1,6 +1,8 @@
+
+
 #import dataset
 import pandas as pd
-df = pd.read_csv("/kc_house_data.csv", dtype = int )
+df = pd.read_csv("/Users/kathi/Desktop/kc_house_data.csv" )
 
 #see columns
 for col in df.columns: print(col)
@@ -8,7 +10,7 @@ for col in df.columns: print(col)
 #outcome variable - price
 
 #correlation matrix between the different variables
-import seaborn as sn
+import seaborn as sns
 import matplotlib.pyplot as plt
 
 # Build correlation matrix
@@ -22,36 +24,34 @@ correlations.columns = ['col1', 'col2', 'correlation']
 
 # Filter for those that contain 'price'
 correlation_price = correlations.query("col1 == 'price' & col2 != 'price'")
+
+#print correlation between predictors and the price columne
 correlation_price.sort_values(by = 'correlation', axis=0, ascending=False)
 
 
-print(type('sqft_living'))
 
 
-
-
-######## linear regression model
+######## model: multiple linear regression
 
 # split dataset in test and train set
-
 from sklearn.linear_model import LinearRegression
 lm = LinearRegression()
 
 # define Z as predictors 
 Z = df [['sqft_living', 'grade', 'sqft_above']]
-lm.fit(Z, df['price'])
 
+#create linear regression model
+lm.fit(Z, df['price'])
 Y_hat = lm.predict(Z)
 
+# Find the R^2
+print('The R-square is: ', lm.score(Z, df['price']))
 
-# residual plot
-import seaborn as sns
-width = 12
-height = 10
-plt.figure(figsize=(width, height))
+# find the mean square error
+from sklearn.metrics import mean_squared_error
+print('The mean square error of price and predicted value using multifit is: ',mean_squared_error(df['price'], Y_hat))
 
 
-ax1 = sns.distplot(df['price'], hist=False, color="r", label="Actual Value")
-sns.displot(Y_hat, hist=False, color="b", label="Fitted Values" , ax=ax1)
 
-plt.plot(Y_hat)
+### apply neural net using Tensor Flow
+# https://www.analyticsvidhya.com/blog/2016/10/an-introduction-to-implementing-neural-networks-using-tensorflow/
